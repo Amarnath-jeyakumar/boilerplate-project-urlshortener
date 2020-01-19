411eb0e319a2ec4435c7d08b0074c7c601daf6e1
Amarnath-jeyakumar <amarnath.jeyakumar@gmail.com>
2020-01-20 00:11:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 var express = require('express');
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var dns = require('dns');
 
 var cors = require('cors');
 
@@ -12,12 +14,25 @@ var app = express();
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-// mongoose.connect(process.env.MONGOLAB_URI);
+//mongoose.connect(process.env.MONGO_URI);
+
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).catch(err=>console.log(err));
+
+let urlSchema = new mongoose.Schema({
+  oldUrl: String,
+  newUrl: Number
+});
+  
+let urlModel = mongoose.model('urlModel', urlSchema);
 
 app.use(cors());
 
 /** this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -31,6 +46,51 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/shorturl/:new", (req, res) => {
+  let shortUrl = {newUrl: req.params.new};
+  
+  urlModel.findOne(shortUrl)
+    .then((data) => res.redirect(data.oldUrl))
+    .catch(() => res.json({"error":"invalid URL"}));
+});
+
+app.post("/api/shorturl/new", (req, res) => {
+  let urlInput = {oldUrl: req.body.url};
+  let newUrl, trimmedUrl;
+  let regexWithS = /https/;
+  let regex = /http/;
+  
+  if(regexWithS.test(urlInput.oldUrl)) {
+    trimmedUrl = urlInput.oldUrl.substring(8);
+  } else if(regex.test(urlInput.oldUrl)) {
+    trimmedUrl = urlInput.oldUrl.substring(7);
+  }
+  
+  dns.lookup(trimmedUrl, (err, address) => {
+    if(err) {
+      res.json({error: 'invalid URL'});
+    } else {
+      urlModel.findOne(urlInput)
+        .then((data) => {
+          res.json({original_url:data.oldUrl, short_url: data.newUrl});
+        })
+        .catch(() => {
+          newUrl = Math.floor((Math.random() * 1000) + 1);
+        
+          var finalObject = new urlModel({
+            oldUrl: urlInput.oldUrl,
+            newUrl: newUrl
+          });
+        
+          finalObject.save()
+            .then(data => {
+              res.json({original_url:data.oldUrl, short_url: data.newUrl});
+            })
+            .catch(err=>console.log(err));
+        });
+    }
+  });
+});
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
